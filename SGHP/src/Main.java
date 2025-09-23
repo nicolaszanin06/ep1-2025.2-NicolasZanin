32ac8420aa6189c046ec4988c3e32f99a040b1bb
Nicolas Zanin <nicolaszanin2006@gmail.com>
2025-09-23 19:01:21 -0300

    Programação inicial da classe Menu + Criação de Classes

--- SGHP/src/Main.java
+++ SGHP/src/Main.java
@@ -1,5 +1,21 @@
+import view.Menu;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Bem-vindo(a) ao sistema de Gerenciamento Hospitalar!");
+        System.out.print("Carregando o menu. \nAguarde");
+        System.out.print(".");
+        //Animação de Loading
+        Thread.sleep(800);
+        System.out.print(".");
+        Thread.sleep(800);
+        System.out.print(".");
+        Thread.sleep(800);
+        System.out.print("");
+
+        Thread.sleep(2000);
+        new Menu();
+        
+        }
     }
-}
+
--- SGHP/src/view/Menu.java
+++ SGHP/src/view/Menu.java
@@ -4,5 +4,33 @@ import java.util.Scanner;
 
 public class Menu {
     Scanner sc = new Scanner(System.in);
-    
+
+    public Menu() {
+        System.out.println("------ Menu Principal ------");
+        System.out.println("1. Gerenciar Pacientes");
+        System.out.println("2. Gerenciar Médicos");
+        System.out.println("3. Gerenciar Consultas");
+        System.out.println("4. Sair");
+        System.out.print("Escolha uma opção: ");
+        int opcao = sc.nextInt();
+        switch (opcao) {
+            case 1:
+                new MenuPaciente();
+                break;
+            case 2:
+                new MenuMedico();
+                break;
+            case 3:
+                new MenuConsulta();
+                break;
+            case 4:
+                System.out.println("Salvando alterações e saindo...");
+                break;
+            default:
+                System.out.println("Opção inválida!");
+                break;
+
+        }
+    }
 }
+
